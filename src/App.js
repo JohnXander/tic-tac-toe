8cb459e61ecfadf58a163e445737e2801d8378d9
John Bloxam <john.bloxam@learningpool.com>
2023-04-12 14:29:50 +0100

    feat: add calculateWinner logic

--- src/App.js
+++ src/App.js
@@ -4,8 +4,31 @@ import Square from './components/Square';
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+  
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 
   const handleClick = (i) => {
+    if (squares[i] || calculateWinner(squares)) {
+    return;
+    }
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
@@ -15,7 +38,7 @@ export default function Board() {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
-
+  
   return (
     <>
       <div className="board-row">
