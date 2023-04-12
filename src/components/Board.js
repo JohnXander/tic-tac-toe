20aca19ac49b45e942d30cbb909aea4e3f8e7779
John Bloxam <john.bloxam@learningpool.com>
2023-04-12 15:13:24 +0100

    feat: add history logic

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,11 +1,7 @@
-import { useState } from 'react';
 import Square from './Square'
 import calculateWinner from '../lib/calculateWinner';
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+export default function Board({ xIsNext, squares, onPlay }) {
   const winner = calculateWinner(squares);
   let status;
 
@@ -25,8 +21,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
   
   return (
