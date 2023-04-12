48fac0dbe0747e81456e2098c75d4c3d71ebcf07
John Bloxam <john.bloxam@learningpool.com>
2023-04-12 14:33:37 +0100

    refactor: store calculateWinner func in lib

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import { useState } from 'react';
 import Square from './components/Square';
+import calculateWinner from './lib/calculateWinner';
 
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
-  
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
 
   const handleClick = (i) => {
     if (squares[i] || calculateWinner(squares)) {
